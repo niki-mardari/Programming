517cf8a9b63e74ac5934b889312f6c7c523221c1
Niki Mardari <nikitamardari@gmail.com>
2026-07-12 00:59:22 +0100

    Fixed bug, first mismatch of values never updated current value

--- C_Prog/Bruh/Encoding/rle0.c
+++ C_Prog/Bruh/Encoding/rle0.c
@@ -1,49 +1,69 @@
-// Basic RLE of a 256 by 256 image
+// Basic rle of a 256 by 256 image
 
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdbool.h>
 #include<stdint.h>
 
-#define readFile "data/current.csv"
-//#define writeFile "data/output.txt"
-//#define width 240
-//#define height 320
+// #define readFile "data/current.csv"
+#define writeFile "rle_output.txt"
+#define width 240
+#define height 320
 
-int main()
-{
-    FILE* input = fopen(readFile, "rb");
-    if(!input) return 1;
+void rle(FILE* input, FILE* output){
 
     char* ptr; // To increment each char
 
-    char line[2048]; // 32 Bytes in 256 bits // 120 in 240 hex numbers
-    int count = 0;
+    char line[2048]; // 2048 byts to store each row, actually need 240*7 bytes for value and comma
+    uint32_t count = 0;
     uint16_t current = -1; // Sentinel value
     uint16_t next;
+    uint32_t pixels = 0;
 
-    for(int row = 0; row < 320; row++){
+    for(int row = 0; row < height; row++){
         fgets(line, sizeof(line), input);
         ptr = line;
         // Debugging
-        // if(row == 2) break;
+        // if(row == 100) break;
 
-        for(int col = 0; col < 240; col++){
+        for(int col = 0; col < width; col++){
             next = (uint16_t)strtol(ptr, &ptr, 16); // Parsing hex value, skipping comma
             ptr++; // To skip the comma location
             
-            if(current == next) count++;
+            if(current == next){
+            count++;
+            }
             else{
                 if(count > 0){
-                    fprintf(stdout, "%d %d\n", current, count);
+                    fprintf(output, "%x %d\n", current, count);
+                    }
+                    pixels += count;
                     current = next;
                     count = 1;
-                    }
                 }  
             }
         }
-    fprintf(stdout, "%d %d\n", current, count);
-    fclose(input);
+    // Reading last row because no next value 
+    fprintf(output, "%x %d\n", current, count);
+    pixels += count;
+    printf("\nDone!\nThe pixel sum is: %d\n", pixels);
+}
 
+int main(int argc, char* argv[])
+{
+    if(argc < 2) perror("\nUsage: rle0.exe <filename>.txt");
+    else{
+    FILE* input = fopen(argv[1], "r");
+    if(!input) return 1;
+
+    FILE* output = fopen(writeFile, "w");
+    if(!output) return 1;
+    
+    rle(input, output);
+    
+    fclose(input);
+    fclose(output);
+
+    }
     return 0;
 }
