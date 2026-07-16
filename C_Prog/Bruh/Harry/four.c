30894367403539638c29aabb83a1f1280a579360
Niki Mardari <nikitamardari@gmail.com>
2026-07-17 00:51:00 +0100

    Stringifying tokens using # and macro define functions

--- C_Prog/Bruh/Harry/four.c
+++ C_Prog/Bruh/Harry/four.c
@@ -4,16 +4,21 @@
 
 // Example blocks: 
 // 8 Bits in each row
+
 // 8 bits is 1 byte
 // 1000 bytes is 1kb
 // 1000kb is 1mb
 // 1024 mb is 1 Gb
 
 // Need a macro in define to convert argument into string
-#define MY_VAR(i, ptr) fprintf(stdout, "\n%s, which is valued at: %d which is at %p location\n", #i, *ptr, (void*)ptr);
+#define MY_VAR(i, ptr) fprintf(stdout, "\nThe variable %s, which is valued at: %d which is at %p location\n", #i, *ptr, (void*)ptr);
 // # in a macro converts it into a string
 // Casting the ptr to a void* because %p specifier expects a generic pointer 
 // Modern compilers are smart enough to correct this like gcc or Clang
+// #define CONCAT(a, b) a ## b // Combine tokens
+// #define TO_STRING(x) #x // Stringify a token
+// #define CONCAT_TO_STRING(a, b) TO_STRING(CONCAT(a, b)) // Merges, then stringifies
+// ## concatenates or merges two tokens into a new single string
 
 // int is 4 bytes
 // char is 1 byte
@@ -21,11 +26,20 @@
 #include<stdio.h>
 
 int main(){
-    int i = 72;
-    int* ptr = &i;
+    int x = 72;
+    int j = 67;
+    int* ptr = &x;
 
-    MY_VAR(i, ptr);
-    printf("\nThe address of i is %lu", &i);
+    MY_VAR(x, ptr);
+    MY_VAR(j, &j);
+
+    // Can also do it this way:
+    printf("\nThe memory address of x is %p", &x);
+    printf("\nThe memory address of j is %p", &j);
+
+    // i and j are 4 bytes adjacent in ram/ stack. int gets allocated 4 bytes
+    fprintf(stdout,"\nThe stack pointer difference is: %lld bytes (1) * (4) bytes", &x - &j); // c automatically assumes pointer is in int (4 bytes)
+    fprintf(stdout,"\nOr %lld bytes\n", (char* )&x - (char* )&j); // Convert to char (1 bytes)
 
     return 0; 
 }
