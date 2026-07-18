b2f27006907cb02c760bb74c347bd73a1def4786
Niki Mardari <nikitamardari@gmail.com>
2026-07-19 00:58:53 +0100

    Passing by reference or by value, pointer arithmetic preactice

--- C_Prog/Bruh/Harry/five.c
+++ C_Prog/Bruh/Harry/five.c
@@ -4,15 +4,42 @@
 
 #include<stdio.h>
 
-int sum(int, int); // Prototype
+int sum(int, int); // Prototype of call by value
+int subtract(int*, int*); // Protoype call by reference 
+void swap(int *x, int *y); // Prototype for call by reference and swapping variables
 
 int main(){
-    printf("\nThe value of a + b = %d", sum(5, 4));
+
+    int x = 5, y = 4;
+    printf("\nThe value of x + y = %d", sum(x, y));
+    printf("\nThe value of x and y are: %d and %d\n", x, y);
+
+    printf("\nThe value of x is passed by reference and changed to 56");
+    printf("\nThe value of x - y = %d", subtract(&x, &y));
+    printf("\nThe value of x and y are: %d and %d\n", x, y);
+
+    swap(&x, &y);
+
     return 0;
 }
 
 // Pass by value, passing a variable by copy 
-int sum(int a, int b){
+int sum(int x, int y){
+    // a = 67;
+    return x + y;
+}
 
-    return a + b;
+// Pass by reference
+int subtract(int *x, int *y){
+    *x = 56;
+    return *x - *y;
+}
+
+void swap(int *x, int *y){
+    int temp = 0;
+    printf("\nValues before swap: x = %d, y = %d, temp = %d\n", *x, *y, temp);
+    temp = *x;
+    *x = *y;
+    *y = temp;
+    printf("\nValues after swap: x = %d, y = %d, temp = %d\n", *x, *y, temp);
 }
---
+++ C_Prog/Bruh/Harry/six.c
@@ -0,0 +1,57 @@
+// Practice exercises done
+// Failed to manipulate a pointer to a pointer k to change the 
+
+#include<stdio.h>
+
+#define MY_VAR(i, ptr) printf("\nThe variable %s is stored in memory location %p and the value is %d\n", #i, ptr, *ptr);
+
+void add(int i, int j){
+    printf("\n i + j = %d\n", i + j);
+    printf("\nIn a pass by value func, the location of i is: %p location of j is: %p\n", &i, &j);    
+}
+
+void multiply(int* j){
+    printf("\nThe variable j is at at address %p and the value stored there is: %d\n", j, *j);
+    *j *= 10;
+    printf("\nThe new value of j is at at address %p and the value stored there is now : %d\n", j, *j);
+    *j /= 10;
+}
+
+void sumavg(int* i, int* j){
+    int sum = *i + *j;
+    float avg = sum / 2.0f;
+    printf("\n i + j = %d, avg = %.2f\n", sum, avg);
+}
+
+void printPointerVal(int** k){
+    printf("\nThe value at pointer address %p is %d\n", *k, **k);
+}
+
+void changePtr(int* ptr){
+    ptr -= 1; 
+    printf("\nThe value that ptr points to now is: %d\n", *ptr);
+}
+
+int main(){
+    int i = 7, j = 2;
+    int* ptr = &i;
+    int **k;
+    k = &ptr;
+
+    MY_VAR(i, ptr);
+    printf("\nIn a pass by reference func, the variable j is at at address %p and the value stored there is: %d\n", &j, j);
+
+    multiply(&j);
+
+    add(i, j);
+
+    sumavg(&i, &j);
+
+    printPointerVal(k);
+    printf("%d", *ptr);
+
+    changePtr(ptr);
+    
+    return 0; 
+}
+
