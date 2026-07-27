ab3a2794f627aed4cec485aaefc6e6271d59b865
Niki Mardari <nikitamardari@gmail.com>
2026-07-27 03:53:34 +0100

    String manipulation, implementing basic functions like strcpy, slice, finding characters...

--- C_Prog/Bruh/Harry/eight.c
+++ C_Prog/Bruh/Harry/eight.c
@@ -75,12 +75,85 @@ int strlen2(char* word){
 }
 
 void slice(char* string, int m, int n){
-    char newString[sizeof(string)];
+    char newString[strlen(string)+1]; // sizof was calculating the size of the pointer 
     for(int i = m; i < n; i++){
-        newString[i] = string[i];
+        newString[i-m] = string[i];
         if(i == (n-1)) newString[i] = '\0';
     }
     printf("\nThe result is: %s", newString);
+}
+
+/* 
+// Need to work on this, returns a local string, it gets removed from the stack frame once the function is over
+char *slice2(char* string, int m, int n){
+    int sliceLength = n - m;
+    char newString[sliceLength - 1];
+
+    int j = 0;
+    for(int i = m; i < n; i++){
+        newString[j] = string[i];
+        j++;
+    }
+    newString[j] = '\0';
+
+    // printf("\nThe result is: %s\n", newString);
+    return newString;
+}
+*/
+
+/*
+void slice3(char* string, int m, int n){
+    // Take string 
+    // slice it and save it in the original string
+    // Say string is 'G' , 'A' , 'F' , 'A' , '\0'
+    // Input Gafa, 0, 2 
+    // Should return GAF
+}
+*/
+
+void myStrcpy(char* dest, char* source){
+    size_t totalSize = strlen(source);
+    for(size_t i = 0; i < totalSize; i++){
+        dest[i] = source[i];
+    }
+    printf("\n\n%s", dest);
+}
+
+void encrypt(char* message){
+    size_t length = strlen(message);
+    for(size_t i = 0; i < length; i++){
+        message[i] ++; 
+    }
+    printf("\nEncrypted message: %s", message);
+} 
+
+void decrypt(char* message){
+    size_t length = strlen(message);
+    for(size_t i = 0; i < length; i++){
+        message[i] --;
+    }
+    printf("\nDecrypted message: %s", message);
+} 
+
+void findOccurence(char* string, char target){
+    size_t length = strlen(string);
+    int count = 0;
+    for(size_t i = 0; i < length; i++){
+        if(string[i] == target) count++;
+    }
+    printf("\nThe letter %c occured %d times", target, count);
+}
+
+void isPresent(char* string, char target){
+    size_t length = strlen(string);
+    int found = 0;
+    for(size_t i = 0; i < length; i++){
+        if(string[i] == target){
+            found = 1;
+            break;
+        }
+    }
+    printf("\n%c is %s in string %s\n", target, found == 1 ? "present" : "Not present", string);
 }
 
 int main(){
@@ -90,6 +163,20 @@ int main(){
 
     char s23[] = "Gafa";
     //printf("\nThe legth of Gafa is: %d",strlen2(s23));
-    slice(s23, 1, 3);
+    slice(s23, 0, 6);
+    //char test[] = "Hello World";
+    //slice2(test, 6, 11);
+    //char* newString = slice2(test, 6, 11);
+    //printf("\nThe result is: %s\n", newString);
+    char dest[] = "Faga";
+    myStrcpy(s23, dest);
+
+    char message[] = "Password";
+    encrypt(message);
+    decrypt(message);
+
+    isPresent(s23, 'a');
+
+
     return 0;
 }
