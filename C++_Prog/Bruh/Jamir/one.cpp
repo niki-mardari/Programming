863673e8c3d803f7e680ff244c5c709cbd939592
Niki Mardari <nikitamardari@gmail.com>
2026-08-01 00:12:24 +0100

    Tested Clang, CMake, fisrt dummy program

---
+++ C++_Prog/Bruh/Jamir/one.cpp
@@ -0,0 +1,18 @@
+// Bruh 
+
+#include<iostream>
+
+void myFunc(){
+    
+    char array[] = {'H','e','l','l','o','W','o','r','d','!',' ','6', '7'};
+    
+    for(auto i {0uz}; i < 13; i++){
+        std::cout << array[i]; 
+    }
+}
+
+int main(){
+
+    myFunc();
+    return 0;
+}
