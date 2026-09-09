651d22f20f17a2cf6bf71502ee1b7a216b08b7bf
Niki Mardari <nikitamardari@gmail.com>
2026-09-09 23:58:59 +0100

    Checking status register, bit masking exercise

---
+++ C_Prog/HackerRank/decode_status_register.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdint.h>
+
+// Predefined bit mask positions 
+#define SENSOR_READY (1U << 0)
+#define OVER_TEMPERATURE (1U << 1)
+#define DATA_AVAILABLE (1U << 2)
+#define BATTERY_LOW (1U << 3)
+#define COMMUNICATION_ERROR (1U << 4)
+#define CALIBRATING (1U << 5)
+// The last 2 bits are apparently reserved
+
+int main(){
+    // Sensor status register
+    uint8_t status = 0b00101101;
+
+    /*
+    // Expected:
+
+    Sensor ready
+    Data available
+    Battery low
+    Calibrating
+    */
+
+    /*
+    Bit 0 → SENSOR_READY
+    Bit 1 → OVER_TEMPERATURE
+    Bit 2 → DATA_AVAILABLE
+    Bit 3 → BATTERY_LOW
+    Bit 4 → COMMUNICATION_ERROR
+    Bit 5 → CALIBRATING
+    Bit 6 → RESERVED
+    Bit 7 → RESERVED
+    */
+
+        if(status & SENSOR_READY) printf("\nSENSOR_READY"); 
+        if (status & OVER_TEMPERATURE) printf("\nOVER_TEMPERATURE");
+        if (status & DATA_AVAILABLE) printf("\nDATA_AVAILABLE"); 
+        if (status & BATTERY_LOW) printf("\nBATTERY_LOW"); 
+        if (status & COMMUNICATION_ERROR) printf("\nCOMMUNICATION_ERROR"); 
+        if (status & CALIBRATING) printf("\nCALIBRATING\n"); 
+
+        return 0;
+}
